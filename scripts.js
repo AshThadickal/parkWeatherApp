8ffad23313694947e77fd440770b0a39a0b27b59
KevinWeber <kevin.ninjabot@gmail.com>
2022-01-04 22:11:28 -0500

    Added catch and throw error handling.

--- scripts.js
+++ scripts.js
@@ -10,19 +10,94 @@
 
 // manipulate the page such that the value from the stored variables will be displayed to the user byway of a template literal with ${variable}
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=ottwa&appid=4310fd1fc9ffb9abc888f8569b40e704')
-    .then(function (response) {
-        if (response.ok) {
-        return response.json();
-        } else {
-            throw new Error(response.statusText)
-        }
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=ottwa&appid=4310fd1fc9ffb9abc888f8569b40e704')
+//     .then(function (response) {
+//         if (response.ok) {
+//         return response.json();
+//         } else {
+//             throw new Error(response.statusText)
+//         }
+//     })
+//     .then(function (jsonResult) {
+//         console.log(jsonResult)
+//         })
+//     .catch((err) => {
+//         if (err.message) {
+//             console.log('we did it')
+//         }
+//     })
+
+
+
+
+
+// 1. Create Namespace Object 
+
+const myApp = {}
+
+
+
+
+// -  Create init method on namespace object
+
+myApp.init = () => {
+    myApp.getWeather ()
+}
+
+// - Add API key as property to our namespace object
+
+myApp.key = ('4310fd1fc9ffb9abc888f8569b40e704')
+myApp.url = ('https://api.openweathermap.org/data/2.5/weather');
+
+// GET DATA!
+myApp.getWeather = () => {
+    const url = new URL (myApp.url);
+    url.search = new URLSearchParams ({
+        q: "Toronto",
+        appid:myApp.key,
     })
-    .then(function (jsonResult) {
-        console.log(jsonResult)
+    fetch (url) 
+        .then ((res) => {
+            if (res.ok === true){
+            return res.json ();
+            } else {
+                throw new Error (res.statusText)
+            }
+                
         })
-    .catch((err) => {
-        if (err.message) {
-            console.log('we did it')
-        }
-    })
+        .then((jsonData) => {
+            console.log(jsonData);
+        })
+        .catch((err) => {
+            if (err.message === "Not Found") {
+            alert("City Not Found");
+            } else {
+            alert("Something went wrong");
+            }
+        })
+    
+
+}
+
+myApp.init();
+
+// - Test API
+
+
+
+// Create new URL object and url search params object!
+
+
+
+// Display Images
+    // Add some HTML based on data from api!
+
+
+    // Get the parent element we want to add on to
+    // Loop through Array and create a new div, and a new image element with our
+    // art URLs
+    // Append our new div to parent element
+
+
+
+// -make sure we call the init method AT THE END
